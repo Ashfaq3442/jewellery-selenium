aa72d4d54d027c3f3a1615800e8c38cd1ec558ca
Ashfaq3442 <ashfaqm46@gmail.com>
2025-01-08 23:07:53 +0500

    Make Changes in my own code

--- jewellery.py
+++ jewellery.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-
-
-
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
@@ -16,6 +13,7 @@ driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()),options
 driver.maximize_window()
 driver.get("https://cullenjewellery.com/engagement-rings")
 for i in range(1,10):
+    price=Review=Ring_Name=Avg=Average_band_width=Carat_Total_Weight=center_stone_size=None
     link=driver.find_element(By.XPATH,f"/html/body/div[1]/div[1]/main/div/div[5]/div[2]/div/div[3]/ul/li[{i}]/a")
     product_url = link.get_attribute("href")
     driver.get(product_url)
@@ -25,38 +23,38 @@ for i in range(1,10):
     try:
         price=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[2]/p").text
     except NoSuchElementException:
-        pass
+        price="N/A"
 
     try:
         Review=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[1]/a/div/div[7]/div").text
     except NoSuchElementException:
-        pass
+        Review="N/A"
 
     try:
         Ring_Name=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/h1").text
     except NoSuchElementException:
-        pass
+        Ring_Name="N/A"
     Detail_find_element=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/button/div[2]").click()
 
     try:
         Avg=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/div/div/table/tr[1]/td").text
     except NoSuchElementException:
-        pass
+        Avg="N/A"
 
     try:
         Carat_Total_Weight=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/div/div/table/tr[2]/td").text
     except NoSuchElementException:
-        pass
+        Carat_Total_Weight="N/A"
 
     try:
         Average_band_width=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/div/div/table/tr[3]/td").text
     except NoSuchElementException:
-        pass
+        Average_band_width="N/A"
 
     try:
         center_stone_size=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/div/div/table/tr[4]/td").text
     except NoSuchElementException:
-        pass
+        center_stone_size="N/A"
     print("................\n\n\n\n")
     print("Ring Name :", Ring_Name)
     print("Price : ",price)
@@ -69,86 +67,3 @@ for i in range(1,10):
     time.sleep(7)
     driver.back()
     time.sleep(3)
-
-
-
-
-
-
-
-
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-# from selenium.common.exceptions import NoSuchElementException
-# import time
-
-# chrome_options = Options()
-# chrome_options.add_experimental_option("detach", True)
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-# driver.maximize_window()
-# driver.get("https://cullenjewellery.com/engagement-rings")
-# # Loop through the products
-# for i in range(1, 10):
-#         link = driver.find_element(By.XPATH, f"/html/body/div[1]/div[1]/main/div/div[5]/div[2]/div/div[3]/ul/li[{i}]/a")
-#         product_url = link.get_attribute("href")
-#         driver.get(product_url)
-
-
-
-#         try:
-#             ring_name = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/h1").text
-#         except NoSuchElementException:
-#             pass
-
-#         try:
-#             price = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[2]/p").text
-#         except NoSuchElementException:
-#             pass
-
-#         try:
-#             review = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[1]/a/div/div[7]/div").text
-#         except NoSuchElementException:
-#             pass
-
-#         detail_button = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/button/div[2]")
-#         detail_button.click()
-        
-
-#         try:
-#             avg = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/div/div/table/tr[1]/td").text
-#         except NoSuchElementException:
-#             pass
-
-#         try:
-#             carat_total_weight = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/div/div/table/tr[2]/td").text
-#         except NoSuchElementException:
-#             pass
-
-#         try:
-#             average_band_width = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/div/div/table/tr[3]/td").text
-#         except NoSuchElementException:
-#             pass
-
-#         try:
-#             center_stone_size = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/main/div/div/div/div[1]/section/div[6]/div[1]/div/div/table/tr[4]/td").text
-#         except NoSuchElementException:
-#             pass
-
-#         # Print the details
-#         print("................\n")
-#         print("Ring Name :", ring_name)
-#         print("Price :", price)
-#         print("Review :", review)
-#         print("Avg. No. Side Stones :", avg)
-#         print("Carat Weight :", carat_total_weight)
-#         print("Average Band Width :", average_band_width)
-#         print("Center Stone Size :", center_stone_size)
-#         print("................\n")
-
-
-#         time.sleep(7)
-#         driver.back()
-#         time.sleep(3)
